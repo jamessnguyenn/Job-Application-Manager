239f656c7afa26d0d678695583bb0a0561e55298
James Nguyen <nguyen.james615@gmail.com>
2020-12-25 00:34:28 -0800

    added additional styling for forms

--- src/App.java
+++ src/App.java
@@ -1,8 +1,11 @@
 import javafx.application.Application;
+import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.SplitPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -13,13 +16,18 @@ public class App extends Application {
 		Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
 		Scene mainScene= new Scene(root);
 		primaryStage.setScene(mainScene);
-		primaryStage.setMaximized(true);
+		//primaryStage.setMaximized(true);
 		primaryStage.initStyle(StageStyle.UNDECORATED);
+		Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+		primaryStage.setX(primaryScreenBounds.getMinX());
+		primaryStage.setY(primaryScreenBounds.getMinY());
+		primaryStage.setWidth(primaryScreenBounds.getWidth());
+		primaryStage.setHeight(primaryScreenBounds.getHeight());
 		primaryStage.show();
 		SplitPane form = (SplitPane) mainScene.lookup("#split-pane");
 		form.lookupAll(".split-pane-divider").stream()
 		.forEach(div -> div.setMouseTransparent(true));
-		
+				
 	}
 	public static void main(String[] args) {
 		launch(args);
