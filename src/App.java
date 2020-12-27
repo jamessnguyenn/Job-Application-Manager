83cc7296b45fdb07c907f63c854ebb8a6dfac1d5
James Nguyen <nguyen.james615@gmail.com>
2020-12-27 02:08:17 -0800

    changed styling of table

--- src/App.java
+++ src/App.java
@@ -1,34 +1,47 @@
+import com.jfoenix.controls.JFXTreeTableView;
+import com.jfoenix.controls.JFXTreeView;
+import com.jfoenix.skins.JFXTableColumnHeader;
+import com.jfoenix.skins.JFXTableHeaderRow;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.SplitPane;
+import javafx.scene.control.skin.TableHeaderRow;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class App extends Application {
+	private Scene mainScene;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-		Scene mainScene= new Scene(root);
+		mainScene = new Scene(root);
 		primaryStage.setScene(mainScene);
 		primaryStage.setMaximized(true);
-		//primaryStage.initStyle(StageStyle.UNDECORATED);
+		// primaryStage.initStyle(StageStyle.UNDECORATED);
 		Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
 		primaryStage.setX(primaryScreenBounds.getMinX());
 		primaryStage.setY(primaryScreenBounds.getMinY());
 		primaryStage.setWidth(primaryScreenBounds.getWidth());
 		primaryStage.setHeight(primaryScreenBounds.getHeight());
 		primaryStage.show();
-		SplitPane form = (SplitPane) mainScene.lookup("#split-pane");
-		form.lookupAll(".split-pane-divider").stream()
-		.forEach(div -> div.setMouseTransparent(true));
+		initializePanes();
 		ResumeSQLHelper db = new ResumeSQLHelper();
 		db.createTable();
 		db.insertIntoTable("Software Intern", "Google", "ksldfllkdafjlkdsafjlkdajflkdsajflkdafjlkdajfladjflkdasfjlkadjfkajflkdajflkdajflkdajflkdajfl", "www.google.com");
 	}
+	private void initializePanes(){
+		SplitPane form = (SplitPane) mainScene.lookup("#split-pane");
+		form.lookupAll(".split-pane-divider").stream().forEach(div -> div.setMouseTransparent(true));
+		JFXTreeTableView treeTableView = (JFXTreeTableView) mainScene.lookup("#application-table");
+		 treeTableView.lookupAll(".column-header").stream().forEach(header->header.setMouseTransparent(true));;
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 		
